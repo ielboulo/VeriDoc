f9ab94c67c7c15b4cfc13db8a7996674c218a664
Arkano.eth <matiasferrer@outlook.com>
2024-01-12 10:57:34 +0100

    Initial commit

---
+++ VeriDoc/target/debug/deps/VeriDoc-d0316e6ea82468f3.d
@@ -0,0 +1,5 @@
+C:\Programación\VeriDoc\VeriDoc\target\debug\deps\libVeriDoc-d0316e6ea82468f3.rmeta: src\lib.rs
+
+C:\Programación\VeriDoc\VeriDoc\target\debug\deps\VeriDoc-d0316e6ea82468f3.d: src\lib.rs
+
+src\lib.rs:
---
+++ VeriDoc/target/debug/deps/VeriDoc-d0f7cfe8174517d7.d
@@ -0,0 +1,5 @@
+C:\Programación\VeriDoc\VeriDoc\target\debug\deps\libVeriDoc-d0f7cfe8174517d7.rmeta: src\lib.rs
+
+C:\Programación\VeriDoc\VeriDoc\target\debug\deps\VeriDoc-d0f7cfe8174517d7.d: src\lib.rs
+
+src\lib.rs:
